af8d33be58663b40cd4113b141e36264475de803
Javierub38 <javierub38@gmail.com>
2025-10-27 13:50:21 +0100

    Lab05 Datos v2

--- Sistemas Intensivos en Datos/lab5/practica5.py
+++ Sistemas Intensivos en Datos/lab5/practica5.py
@@ -4,6 +4,9 @@ from pymongo import MongoClient
 def crear_conexion():
 	client = MongoClient("mongodb://admin:admin@localhost:27017/")
 	db = client["pr05"]
+	db.cursos.drop()
+	db.estudiantes.drop()
+	db.calificaciones.drop()
 	return client, db
 
 def insertar_cursos(db):
@@ -72,6 +75,28 @@ def insertar_calificaciones(db):
 	{"estudiante_id": 3, "curso_id": 1, "calificacion": 7.5},
 	{"estudiante_id": 4, "curso_id": 3, "calificacion": 8.0}]
 	db.calificaciones.insert_many(calificaciones)
+ 
+def ejercicio1_consultaEstudiantes(db):
+	resultados = db.estudiantes.find({}, {"_id": 0})
+	print("Tipo de dato de resultados:", type(resultados))
+	
+	for estudiante in resultados:
+		print(estudiante["nombre"] + " - " + estudiante["email"])
+
+	listado_resultados = list(resultados)
+	print("Tipo de dato de listado_resultados:", type(listado_resultados))
+
+def consultas_madrid(db):
+	resultados = db.estudiantes.find({"ciudad": "Madrid"}, {"_id": 0, "nombre": 1, "ciudad": 1})
+	for estudiante in resultados:
+		print(estudiante["nombre"] + " - " + estudiante["ciudad"])
+
+def consultar_cursos_mas_50Horas(db):
+	resultados = db.cursos.find({"duracion_horas": {"$gt": 50}}, {"_id": 0, "nombre": 1, "duracion_horas": 1})
+	for curso in resultados:
+		print(curso["nombre"] + " - " + str(curso["duracion_horas"]) + " horas")
+
+
 
 if __name__ == "__main__":
 	try:
@@ -79,7 +104,10 @@ if __name__ == "__main__":
 		insertar_cursos(db)
 		insertar_estudiantes(db)
 		insertar_calificaciones(db)
-		print("Documentos insertados correctamente.")
+
+		ejercicio1_consultaEstudiantes(db)
+		consultas_madrid(db)
+		consultar_cursos_mas_50Horas(db)
 	except Exception as e:
 		print("Error al insertar documentos:", e)
 	finally:
